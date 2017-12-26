fe490fedc33a009adea79730808be1c962e45a7a
ac065455 <chen.anthony.1109@gmail.com>
2017-12-26 10:30:55 -0500

    removed card

--- src/components/card.js
+++ src/components/card.js
@@ -4,16 +4,8 @@ const Card = (props) => {
 	return (
 		<div className="card">
 		  <div className="card-block">
-		  	<div className="card-top">
-		    	<h4 className="card-title">{props.foodName}</h4>
-		    </div>
-
 		    <div>
 		    	<img className="card-img" src={props.foodPic} alt={props.foodPic} />
-		    </div>
-
-		    <div className="card-bottom">
-		    	<p className="card-text">{props.foodText}</p>
 		    </div>
 		  </div>
 		</div>
