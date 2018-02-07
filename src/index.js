76098189c2587b911e7456c13af473afe427cf16
ac065455 <chen.anthony.1109@gmail.com>
2018-02-07 15:51:15 -0500

    customer wants new design

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/css/styles.css';
-
-//ROUTES
-import Routes from './routes/routes';
-
-ReactDOM.render(<Routes />, document.getElementById('root'));
